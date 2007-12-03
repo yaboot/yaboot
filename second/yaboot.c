66ffb9d32d2acb3613594ef492d341b5bacf02e5
Paul Nasrat <pauln@truemesh.com>
2007-12-03 14:13:54 +0000

    Fix typo in patch porting

--- second/yaboot.c
+++ second/yaboot.c
@@ -1729,7 +1729,7 @@ yaboot_main(void)
 	  prom_printf("%s: Unable to parse\n", bootdevice);
 	  return -1;
      }
-     if (_machine == MACH_bplan)
+     if (_machine == _MACH_bplan)
         boot.part++;
      DEBUG_F("After parse_device_path: dev=%s, part=%d, file=%s\n",
 	     boot.dev, boot.part, boot.file);
