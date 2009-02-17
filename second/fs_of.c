1110d149dc41114e22297244ca274b9a52487356
Tony Breeds <tony@bakeyournoodle.com>
2009-02-18 10:20:12 +1100

    Increase the LOAD_BUFFER_SIZE to 20MiB.
    As installers add more features initrd's get bigger.  Lets try to
    accommodate them.
    
    Signed-off-by: Tony Breeds <tony@bakeyournoodle.com>

--- second/fs_of.c
+++ second/fs_of.c
@@ -45,7 +45,7 @@
 #include "debug.h"
 
 #define LOAD_BUFFER_POS		0x1000000
-#define LOAD_BUFFER_SIZE	0x1000000
+#define LOAD_BUFFER_SIZE	0x1400000
 
 static int of_open(struct boot_file_t* file, const char* dev_name,
 		   struct partition_t* part, const char* file_name);
